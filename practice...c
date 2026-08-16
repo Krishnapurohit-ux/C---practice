2e3e4043b2a4786f1a57fa1facca3249ec9eb32c
krishna purohit <purohitkrishnapu1779@gmail.com>
2026-08-16 11:08:03 +0530

    add C practice files

---
+++ practice...c
@@ -0,0 +1,922 @@
+#include <stdio.h>
+int main() {
+    // int number;
+    // printf("Enter a number: ");
+    // scanf("%d", &number);
+
+    // if(number > 0) {
+    //     printf("The number is positive");
+    // } else if(number < 0) {
+    //     printf("The number is negative");
+    // } else {
+    //     printf("The number is zero");
+    // }
+
+   // FIRST QUESTION
+
+
+    //int number;
+    //printf("Enter a number: ");
+    //scanf("%d", &number);
+    //if (number % 2 == 0) {
+       // printf("The number is even");
+    //} else {
+       // printf("The number is odd");
+    //}
+
+     // SECOND QUESTION
+
+    
+
+
+     //int age ;
+     //printf("Enter your age: ");
+       // scanf("%d", &age);
+        //if (age >= 18) {
+          //  printf("You are an adult.");
+        //} else {
+          //  printf("You are a minor.");
+        //}
+
+
+        // THIRD QUESTION
+
+
+
+
+        //int number;
+        //printf("Enter a number: ");
+        //scanf("%d", &number);
+        //if(number > 100) {
+          //  printf("The number is greater than 100");
+        //} else if(number < 100) 
+        //{
+          //  printf("the number is less than 100");
+        //}
+        
+       // FOURTH QUESTION
+
+
+       // char letter;
+        //printf("Enter a letter: ");
+        //scanf(" %c", &letter);
+         
+        //if(letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u')
+        //{
+          //  printf("The letter is a vowel");
+
+        //}
+
+        //else 
+        //{
+          //  printf("The letter is a consonant");
+        //}
+        
+        //FIFTH QUESTION
+
+        //int number;
+        //printf("Enter a number: ");
+        //scanf("%d", &number);
+
+        //if(number % 5 == 0)
+        //{
+          //  printf("The number is divisible by 5");
+        //}
+        //else
+        //{
+          //  printf("The number is not divisible by 5");
+        //}
+    
+
+        //SIXTH QUESTION
+
+
+
+        //int number;
+        //printf("Enter a number: ");
+        //scanf("%d", &number);
+
+        //if(number % 7 == 0)
+        //{
+          //  printf("The number is divisible by 7");
+        //}
+        //else
+        //{
+          //  printf("the number is not divisible by 7");
+        //}
+
+
+       //SEVENTH QUESTION
+
+        
+
+
+ 
+
+        //int number;
+        //printf("Enter a number: ");
+        //scanf("%d", &number);
+
+        //if(number >= 33)
+        //{
+          //printf("passed");
+        //}
+        //else if (number < 33)
+        //{
+          //printf("failed");
+        //}
+        
+       // EIGHTH QUESTION
+
+
+        //int salary;
+        //printf("Enter your salary: ");
+        //scanf("%d", &salary);
+
+        //if(salary > 50000)
+        //{
+          //printf("salary taxable");
+        //}
+        //else if (salary < 50000)
+        //{
+          //printf("salary not taxable");
+        //}
+
+        // NINTH QUESTION
+
+
+
+       // int temperature;
+        //printf("Enter the temperature: ");
+        //scanf("%d", &temperature);
+
+        //if(temperature > 30)
+        //{
+          //  printf("It's hot outside");
+        //}
+        //else if (temperature <= 30)
+        //{
+          //  printf("It's normal outside");
+        //}
+
+
+
+       //TENTH QUESTION
+
+
+
+      
+      //  int a;
+      //  int b;
+        
+      //  printf("Enter a: ");
+      //  scanf("%d", &a);
+      //  printf("Enter b: ");
+      //  scanf("%d", &b);
+
+
+
+       // if(a > b)
+       // {
+         //   printf("a is greater than b");
+       // }
+        //else if (a < b)
+        //{
+         //   printf("b is greater than a");
+        //}
+
+
+
+
+     //ELEVENTH QUESTION
+
+      
+
+    //  int a;
+    //  int b;
+        
+     //   printf("Enter a: ");
+      //  scanf("%d", &a);
+      //  printf("Enter b: ");
+      //  scanf("%d", &b);
+
+       // if(a == b)
+       // {
+       //     printf("a is equal to b");
+      //  }
+
+        //twelfth question
+
+
+    //    int a;
+    //    int b;
+     //   int c;
+
+    //    printf("Enter a: ");
+     //   scanf("%d", &a);
+    //    printf("Enter b: ");
+    //    scanf("%d", &b);
+    //    printf("Enter c: ");
+    //    scanf("%d", &c);
+
+
+     //   if(a > b && a > c)
+      //  {
+      //      printf("a is the greatest");
+      //  }
+      //  else if (b > a && b > c)
+      //  {
+      //      printf("b is the greatest");
+      //  }
+      //  else if (c > a && c > b)
+     //   {
+       //     printf("c is the greatest");
+       // }
+        
+      //thirteen question
+
+
+
+
+  //    int a;
+  //    int b;
+  //    int c;
+
+  //    printf("Enter a:");
+  //    scanf("%d", &a);
+  //    printf("Enter b: ");
+  //    scanf("%d", &b);
+  //    printf("Enter c: ");
+  //    scanf("%d", &c)//;
+//
+
+  //    if(a < b && a < c)
+    //  {
+      //  printf("a is the smallest");
+     // }
+     // else if (b < a && b < c)
+     //// {
+     //   printf("b is the smallest");
+     // }
+     // else if (c < a && c < b)
+     // {
+     //   printf("c is the smallest");
+      //}
+
+
+      //fourteen question
+
+
+
+
+    //  int number;
+     // printf("Enter a number: ");
+     //   scanf("%d", &number);
+
+      //  if(number % 3 == 0 && number % 5 == 0)
+      //  {
+      //      printf("The number is divisible by both 3 and 5");
+      //  }
+      //  else
+      //  {
+        //    printf("The number is not divisible by both 3 and 5");
+       // }
+
+
+
+       //sixteen question
+
+
+
+   //   int number;
+   //   printf("Enter a number: ");
+   //   scanf("%d", &number);
+
+   //     if(number % 2 == 0 || number % 3 == 0)
+   //     {
+   //         printf("The number is divisible by either 2 or 3");
+     //   }
+     //   else
+      //  {
+      //      printf("The number is not divisible by either 2 or 3");
+      //  }
+
+
+       //seventeen question  
+
+
+
+
+
+    //   char letter;
+    //    printf("Enter a letter: ");
+    //      scanf(" %c", &letter);
+  
+     //     if(letter >= 'A' && letter <= 'Z')
+      //    {
+      //        printf("The letter is uppercase");
+       //   }
+      //    else if(letter >= 'a' && letter <= 'z')
+       //   {
+        //      printf("The letter is lowercase");
+        //  }
+
+
+        //eighteen question
+
+
+
+    //    char letter;
+    //    printf("Enter a letter: ");
+     //   scanf(" %c", &letter);
+
+     //     if((letter >= 'A' && letter <= 'Z') || (letter >= 'a' && letter <= 'z'))
+      //    {
+       //       printf("The letter is an alphabet");
+       //   }
+       //   else
+        //  {
+        //      printf("The letter is not an alphabet");
+        //  }
+          
+         
+        //nineteen question
+
+      
+  
+    //   int number;
+  
+    //    printf("Enter your number: ");
+   //     scanf("%d", &number);
+
+    //    if(number >= 90 && number <= 100)
+     //   {
+     //     printf("A");
+     //   }
+     //   else if(number >= 80 && number <= 89)
+    //    {
+    //      printf("B");
+    //    }
+    //    else if(number >= 70 && number <= 79)
+    //    {
+    //      printf("C");
+    //    }
+    //    else if(number >= 60 && number <= 69)
+    //    {
+    //      printf("D");
+    //    }
+    //    else if(number >= 0 && number <= 59)
+     //    { printf("F");
+      //  }
+        
+      //twentyone question
+
+
+
+   //   int month;
+   //   printf("Enter a month number (1-12): ");
+   //     scanf("%d", &month);
+      
+   //     if(month == 1)
+   //     {
+   //       printf("january");
+
+   //     }
+   //     else if (month == 2)
+   //     {
+   //       printf("February");
+   //     }
+   //     else if (month == 3)
+   //     {
+   //       printf("March");
+   //     }
+   //     else if (month == 4)
+   //     {
+    //      printf("April");
+    //    }
+    //    else if (month == 5)
+    //    {
+    //      printf("May");
+    //    }
+    //    else if (month == 6)
+    //    {
+    //      printf("June");
+     //   }
+    //    else if (month == 7)
+     //   {
+     //     printf("July");
+    //    }
+    //    else if (month == 8)
+    //    {
+    //      printf("August");
+    //    }
+    //    else if (month == 9)
+    //    {
+    //      printf("September");
+    //    }
+    //    else if (month == 10)
+    //    {
+    //      printf("October");
+    //    }
+    //    else if (month == 11)
+    //    {
+    //      printf("November");
+    //    }
+    //     else if (month == 12)
+    //     {
+    //       printf("December");
+    //     }
+
+
+    //twentytwo question
+
+
+
+ //   int number;
+ //   printf("Enter number: ");
+ //   scanf("%d", &number);
+
+ //   if(number < 100)
+  //  {
+  //    printf("Rs5/units");
+   // }
+   // else if(number == 100-200)
+   // {
+    //  printf("Rs7/units");
+  //  }
+  //  else if(number > 200)
+  //  {
+   //   printf("Rs10/units");
+   // }
+        
+   //23 question
+
+
+
+  
+//    float salary, bonus;
+
+//    printf("Enter Salary: ");
+//    scanf("%f", &salary);
+
+//    if(salary > 100000)
+//    {
+//        bonus = salary * 20 / 100;
+//    }
+//    else if(salary > 50000)
+//    {
+ //       bonus = salary * 10 / 100;
+ //   }
+//    else
+//    {
+//        bonus = salary * 5 / 100;
+ //   }
+
+//    printf("Bonus = %.2f", bonus);
+
+
+      //24 question
+
+
+
+
+//    float bmi;
+
+//    printf("Enter BMI: ");
+//    scanf("%f", &bmi);
+
+ //   if(bmi < 18.5)
+ //   {
+  //      printf("Underweight");
+ //   }
+ //   else if(bmi >= 18.5 && bmi < 25)
+ //   {
+  //      printf("Normal");
+ //   }
+ //   else if(bmi >= 25 && bmi < 30)
+ //   {
+ //       printf("Overweight");
+ //   }
+ //   else
+ //   {
+  //      printf("Obese");
+ //   }
+
+
+    //25 question
+
+    
+    
+//    int a;
+//    int b;
+//    int c;
+
+//    printf("Enter a: ");
+//    scanf("%d", &a);
+//    printf("Enter b: ");
+//    scanf("%d",&b);
+//    printf("Enter c: ");
+//    scanf("%d",&c);
+
+
+//    if(a == b && b == c)
+ //   {
+ //       printf("Equilateral Triangle");
+   // }
+  //  else if(a == b || b == c || a == c)
+  //  {
+    //    printf("Isosceles Triangle");
+   // }
+  //  else
+  //  {
+  //      printf("Scalene Triangle");
+  //  }
+
+    
+    //28 question
+
+    
+//    int cost_price;
+//    int selling_price;
+
+//    printf("Enter Cost Price: ");
+//    scanf("%d", &cost_price);
+
+//    printf("Enter Selling Price: ");
+//    scanf("%d", &selling_price);
+
+ //   if(selling_price > cost_price)
+ //   {
+ //       printf("Profit = %d", selling_price - cost_price);
+ //   }
+ //   else if(cost_price > selling_price)
+ //   {
+  //      printf("Loss = %d", cost_price - selling_price);
+  //  }
+ //   else
+  //  {
+    //    printf("No Profit No Loss");
+   // }
+
+
+   //29 question
+
+  
+//    int a;
+//    int b;
+//    int choice;
+
+//    printf("Enter First Number: ");
+//    scanf("%d", &a);
+
+//    printf("Enter Second Number: ");
+//    scanf("%d", &b);
+
+//    printf("\n1. Addition");
+//    printf("\n2. Subtraction");
+//    printf("\n3. Multiplication");
+//    printf("\n4. Division");
+//    printf("\n5. Modulus");
+
+//    printf("\nEnter choice: ");
+//    scanf("%d", &choice);
+
+//    if(choice == 1)
+//    {
+//        printf("Answer = %d", a + b);
+//    }
+//    else if(choice ==  2)
+//    {
+//        printf("Answer = %d", a - b);
+//    }
+//    else if(choice == 3)
+//    {
+//        printf("Answer = %d", a * b);
+//    }
+//    else if(choice == 4)
+//    {
+//        printf("Answer = %d", a / b);
+//    }
+//    else if(choice == 5)
+ //   {
+ //       printf("Answer = %d", a % b);
+ //   }
+    
+
+ //31 question
+
+  
+//    int a;
+//    int b;
+//    int c;
+//    int d;
+//    int e;
+
+//    printf("Enter a: ");
+//    scanf("%d", &a);
+//    printf("Enter b: ");
+//    scanf("%d",&b);
+//    printf("Enter c: ");
+//    scanf("%d", &c);
+//    printf("Enter d: ");
+//    scanf("%d", &d);
+//    printf("Enter e: ");
+//    scanf("%d", &e);
+
+//    if(a >= b && a >= c && a >= d && a >= e)
+//    {
+//        printf("%d is the greatest number.", a);
+//    }
+//    else if(b >= a && b >= c && b >= d && b >= e)
+//    {
+//        printf("%d is the greatest number.", b);
+//    }
+//    else if(c >= a && c >= b && c >= d && c >= e)
+//    {
+ //       printf("%d is the greatest number.", c);
+ //   }
+ //   else if(d >= a && d >= b && d >= c && d >= e)
+//    {
+//        printf("%d is the greatest number.", d);
+//    }
+//    else
+//    {
+ //       printf("%d is the greatest number.", e);
+ //   }
+
+
+ // 32 question
+
+
+
+//    int a;
+//    int b;
+//    int c;
+//    int d;
+
+//    printf("Enter a: ");
+//    scanf("%d", &a);
+
+//    printf("Enter b: ");
+//    scanf("%d", &b);
+
+//    printf("Enter c: ");
+//    scanf("%d", &c);
+
+//    printf("Enter d: ");
+//    scanf("%d", &d);
+
+//    if(a >= b && a >= c && a >= d)
+//    {
+//        printf("%d is the largest number.", a);
+//    }
+//    else if(b >= a && b >= c && b >= d)
+//    {
+//        printf("%d is the largest number.", b);
+//    }
+//    else if(c >= a && c >= b && c >= d)
+//    {
+//        printf("%d is the largest number.", c);
+//    }
+//    else
+//    {
+ //       printf("%d is the largest number.", d);
+ //   }
+
+ //26 question
+
+//   int age;
+//   printf("Enter your age: ");
+//   scanf("%d", &age);
+
+//   if(age < 18)
+//   {
+//      printf("Minor");
+//   }
+//   else if (age >= 18)
+//   {
+//    printf("Adult");
+//   }
+//   else if(age > 50)
+//   {
+//    printf("Senior citizen");
+//   }
+
+
+//33 question
+
+
+//    float bill;
+//    float discount;
+//    float final_Amount;
+
+//    printf("Enter Bill Amount: ");
+//    scanf("%f", &bill);
+
+ //   if(bill > 10000)
+//    {
+//        discount = bill * 20 / 100;
+//    }
+//    else if(bill > 5000)
+//    {
+//        discount = bill * 10 / 100;
+ //   }
+ //   else
+ //   {
+//        discount = bill * 5 / 100;
+//    }
+
+//    final_Amount = bill - discount;
+
+//    printf("Discount = %.2f\n", discount);
+//    printf("Final Amount = %.2f", final_Amount);
+
+    
+ // 34 question
+
+ 
+//    int age;
+
+//    printf("Enter Your Age: ");
+//    scanf("%d", &age);
+
+//    if(age < 5)
+//    {
+//        printf("Ticket Price = FREE");
+//    }
+//    else if(age <= 12)
+//    {
+//        printf("Ticket Price = Rs.120");
+//    }
+//    else if(age <= 50)
+//    {
+//        printf("Ticket Price = Rs.220");
+//    }
+//    else
+ //   {
+  //      printf("Ticket Price = Rs.130");
+  //  }
+
+    
+  // 35  question
+
+  
+//      int a;
+//      int b;
+//      int c;
+//      printf("Enter a: ");
+//      scanf("%d", &a);
+//      printf("Enter b: ");
+//      scanf("%d", &b);
+//      printf("Enter c: ");
+//      scanf("%d", &c);
+
+//      if (a + b > c && b + c > 2 && a + c > b)
+//      {
+//        printf("traingle is valid");
+//      }
+//      else{
+//        printf("triangle is not valid");
+//      }
+
+
+
+  //27 question
+
+
+
+
+
+
+
+//    int year;
+
+//    printf("Enter Year: ");
+//    scanf("%d", &year);
+
+//    if(year % 4 == 0)
+//    {
+//        printf("Leap Year");
+//    }
+//    else
+//    {
+//        printf("Not a Leap Year");
+//    }
+
+
+
+//15 question
+
+
+
+
+//    int salary;
+
+//    printf("Enter your monthly salary: ");
+//    scanf("%d", &salary);
+
+//    if (salary >= 4000)
+//    {
+ //       printf("Eligible for bank loan");
+//    }
+//    else
+//    {
+//        printf("Not eligible for bank loan");
+//    }
+
+  //bank loan
+
+
+  
+//    int amount;
+
+ //   printf("Enter recharge amount: ");
+ //   scanf("%d", &amount);
+
+//    if (amount == 299)
+//    {
+//        printf("28 Days Validity");
+//    }
+//    else if (amount == 399)
+//    {
+//        printf("56 Days Validity");
+ //   }
+ //   else if (amount == 499)
+ //   {
+ //       printf("84 Days Validity");
+ //   }
+ //   else
+ //   {
+ //       printf("Invalid Recharge Plan");
+ //   }
+
+ //recharge
+
+
+  
+ //   int amount;
+
+//    printf("Enter shopping amount: ");
+//    scanf("%d", &amount);
+
+//    if (amount >= 1000)
+//    {
+//        printf("40%% Discount");
+//    }
+//    else if (amount >= 500)
+//    {
+//        printf("20%% Discount");
+//    }
+//    else
+ //   {
+ //       printf("No Discount");
+  //  }
+
+  //online shopping
+
+    
+  //  int age;
+
+//    printf("Enter your age: ");
+//    scanf("%d", &age);
+
+//    if (age < 5)
+//    {
+//        printf("Ticket Fare = Rs.0 (Free)");
+//    }
+//    else if (age <= 12)
+ //   {
+  //      printf("Ticket Fare = Rs.100");
+ //   }
+ //   else if (age < 60)
+  //  {
+ //       printf("Ticket Fare = Rs.250");
+  //  }
+  //  else
+  //  {
+  //      printf("Ticket Fare = Rs.150");
+  //  }
+//ticket
+    
+
+
+    
+
+
+
+
+
+
+    
+
+
+    return 0;
+
+  }        
+
+  
+
+
+
+
+
---
+++ practice2...c
@@ -0,0 +1,441 @@
+#include <stdio.h>
+int main(){
+ //   int i;
+ //   for(i = 1; i <= 10; i ++)
+  //  {
+  //      printf("%d\n", i);
+ //   }
+
+// 1 question
+
+
+//   int i;
+
+//   for(i = 10; i >= 1; i--)
+//   {
+//    printf("%d\n",i);
+//   }
+// 2 question
+
+
+//    int i;
+
+//    for(i = 1; i <= 100; i++)
+//    {
+//        if(i % 2 == 0)
+//        {
+//            printf("%d\n", i);
+//        }
+//    }
+
+//3 question
+
+
+//   int i;
+//   for(i = 1; i <= 100; i++)
+//   {
+//    if(i % 2 == 1)
+//    {
+//        printf("%d\n", i);
+//    }
+//   }
+//4 question
+
+ //   int i;
+ //   for(i = 1; i <= 10; i++)
+ //   {
+ //      printf("5 * %d = %d\n", i , 5 * i);
+ //   }
+// 5 question
+
+ //   int i;
+//    for(i = 1; i <= 10; i++)
+//    {
+//        printf("%d\n",i);
+//    }
+//6 question
+
+
+
+//    int n;
+//    int  i;
+//    int  sum = 0;
+
+ //   printf("Enter a number: ");
+ //   scanf("%d", &n);
+
+//    for(i = 1; i <= n; i++)
+//    {
+//        sum = sum + i;
+    //}
+
+ //   printf("Sum = %d", sum);
+ //9 question
+
+
+
+ //   int n;
+//    int i;
+ //   int product = 1;
+
+//    printf("Enter a number: ");
+//    scanf("%d", &n);
+
+//    for(i = 1; i <= n; i++)
+//    {
+//        product = product * i;
+//    }
+
+//    printf("Product = %d", product);
+//10question
+
+//int i;
+//int number;
+//int n;
+
+//printf("Enter number: ");
+//scanf("%d",&n);
+
+// for(i = 1; i <= n; i++ )
+// {
+//    printf("%d square = %d\n",i, i * i);
+// }
+ //11question
+
+// int i;
+ //int number;
+// int n;
+
+// printf("Enter number: ");
+// scanf("%d", &n);
+
+// for(i = 1; i <= n; i++)
+ //{
+ //   printf("%d cube = %d\n" , i , i * i * i);
+ //}
+ //12question
+
+
+//    int i;
+    
+ //   for(i = 1; i <= 100; i++)
+//    {
+//        if(i % 3 == 0)
+//        {
+//            printf("%d\n", i);
+//        }
+ //   }
+ //13question
+
+
+//   int i;
+
+//   for(i = 1; i <= 100; i++)
+//   {
+//      if(i % 5 == 0)
+//      {
+//        printf("%d\n", i);
+ //     }
+ //  }
+//14question
+
+
+ //  char i;
+ //  for(i = 'A'; i <= 'Z'; i++)
+ //  {
+ //   printf("%c\n", i);
+ //  }
+ //15question
+
+   
+//    int i;
+//    int n;
+//    int sum = 0;
+    
+//    printf("Enter number: ");
+//    scanf("%d",n);
+
+//    for(i = 1; i <= n; i++)
+//    {
+//         if (i % 2 == 0)
+//         {
+//            sum = sum + i;
+ //        }
+ //   }
+
+      
+ //    printf("Sum = %d", sum);
+ //18question problem answer not coming
+
+
+
+//    int i;
+//    int n;
+ //   int number;
+ //   int sum;
+   
+//    printf("Enter number: ");
+//    scanf("%d", n);
+     
+//    for(i = 1; i <= n; i++)
+//    {
+//      if(i % 2 == 1)
+ //     {
+ //        sum = sum + i;
+ //     }
+ //   }
+ //    printf("Sum = %d", sum);
+//19question problem answer not coming
+
+
+   
+ //   int i;
+//    int n;
+
+//    printf("Enter number: ");
+ //   scanf("%d", &n);
+
+ //   for(i = n; i >= 1; i--)
+ //   {
+ //       printf("%d\n", i);
+ //   }
+ //20question
+
+
+//    int i;
+
+ //   for(i = 10; i >= 1; i--)
+//    {
+//        printf("5 * %d = %d\n", i, 5 * i);
+//    }
+//21question
+   
+   
+//    int number;
+//    int i;
+//    int prime = 1;
+
+//    printf("Enter a number: ");
+ //   scanf("%d", &number);
+
+ //   if (number <= 1)
+ //   {
+ //       printf("%d is not a Prime Number.", number);
+ //   }
+ //   else
+  //  {
+  //      for (i = 2; i < number; i++)
+  //      {
+  //          if (number % i == 0)
+ //           {
+ //               prime = 0;
+ //           }
+   //     }
+
+    //    if (prime == 1)
+    //    {
+    //        printf("%d is a Prime Number.", number);
+   //     }
+   //     else
+   //     {
+   //         printf("%d is not a Prime Number.", number);
+   //     }
+   // }
+
+    //24question
+
+    //22,23,25,27,26 doubt
+
+
+
+    
+   // int number;
+   // int reverse = 0;
+   // int digit;
+
+ //   printf("Enter number: ");
+ //   scanf("%d", &number);
+
+ //   if(number == 0)
+  //  {
+//        printf("Reverse = 0");
+//    }
+ //   else
+ //   {
+   //     while(number != 0)
+  //      {
+  //          digit = number % 10;
+  //          reverse = reverse * 10 + digit;
+   //         number = number / 10;
+   //     }
+
+        //printf("Reverse = %d", reverse);
+   // }
+
+    //29question
+
+
+
+ //   int i;
+//    int number;
+//    int digit;
+//    int sum;
+//    printf("Enter a number: ");
+//    scanf("%d",&number);
+
+//    for(number != 0; number = number / 10;)
+ //   {
+ //       digit = number % 10;
+ //       sum = sum + digit;
+ //   }
+ //   printf("Sum = %d", sum);
+//31 question answer error
+
+
+//   int number;
+//   int i;
+//   int product;
+//   int digit;
+  
+  
+//   printf("Enter number: ");
+//   scanf("%d", &number);
+
+//    for(number != 0; number = number / 10;)
+//    {
+//      digit = number % 10;
+//      product = product * digit;
+
+ //   }
+
+ //   printf("product = %d", product);
+//32question answer error
+
+
+
+//    int number;
+//    int original;
+//    int i ;
+//    int digit;
+//    int sum;
+
+
+
+//    printf("Enter number: ");
+//    scanf("%d", &number);
+ //   for(number != 0; number = number / 10;)
+ //   {
+//      digit = number % 10;
+//      sum = sum + ( digit * digit * digit);
+ //   }
+
+ //   if(original == sum)
+ //   {
+ //     printf("%d it is an armstrong number");
+ //   }
+ //   else{
+ //     printf("%d it is not armstrong number");
+ //   }
+//34question answer error
+
+
+
+    
+//    int number;
+//    int  i;
+//    int sum = 0;
+
+//    printf("Enter number: ");
+//    scanf("%d", &number);
+//
+ //   for(i = 1; i < number; i++)
+ //   {
+ //       if(number % i == 0)
+ //       {
+ //           sum = sum + i;
+ //       }
+ //   }
+
+ //   if(sum == number)
+ //   {
+ //       printf("%d is a Perfect Number.", number);
+   // }
+ //   else
+ //   {
+  //      printf("%d is not a Perfect Number.", number);
+  //  }
+  //35question
+
+
+
+  
+//    int a;
+//    int b;
+//    int i;
+//    int hcf = 1;
+
+//    printf("Enter first number: ");
+//    scanf("%d", &a);
+
+//    printf("Enter second number: ");
+//    scanf("%d", &b);
+
+//    for(i = 1; i <= a && i <= b; i++)
+//    {
+//        if(a % i == 0 && b % i == 0)
+//        {
+//            hcf = i;
+//        }
+//    }
+
+ //   printf("HCF = %d", hcf);
+ //37 question
+
+// int a;
+// int b;
+// int i;
+// int lcm;
+
+// printf("Enter first number: ");
+// scanf("%d",&a);
+// printf("Enter second number: ");
+// scanf("%d",&b);
+
+// for(i =1; i <= a * b; i++)
+// {
+//  if(i % a == 0 && i % b == 0 && lcm == 0)
+ // {
+//    lcm = i;
+//  }
+// }
+ //printf("LCM = %d", lcm);
+//38question
+
+
+//39 doubt
+
+
+ //   int number;
+ //   int digit;
+
+ //   printf("Enter number: ");
+ //   scanf("%d", &number);
+
+ //   for(; number != 0; number = number / 10)
+ //   {
+ //       digit = number % 10;
+ //       printf("%d\n", digit);
+ //   }
+ //40question
+
+
+    
+
+  
+    
+
+  
+
+
+     return 0;
+}
