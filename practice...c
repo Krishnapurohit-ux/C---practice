f32cadd8eb2ff7d3a46e1fe83dfe2c2d89778439
Krishnapurohit-ux <purohitkrishnapu1779@gmail.com>
2026-08-20 18:01:39 +0530

    update practice

--- practice...c
+++ practice...c
@@ -898,13 +898,50 @@ int main() {
   //  }
 //ticket
     
-
+//project
+   
 
     
+    int marks[5];
+    int total = 0;
+    float percentage;
 
+    printf("Enter marks of 5 subjects:\n");
+    
+    for(int i = 0; i < 5; i++)
+    {
+        printf("Subject %d: ", i + 1); 
+        scanf("%d", &marks[i]);
+        total = total + marks[i];
+    }
 
+    percentage = total / 5.0;
 
+    if(percentage >= 90)
+    {
+        printf("Grade: A+\n");
+    }
+    else if(percentage >= 80)
+    {
+        printf("Grade: A\n");
+    }
+    else if(percentage >= 70)
+    {
+        printf("Grade: B\n");
+    }
+    else if(percentage >= 60)
+    {
+        printf("Grade: C\n");
+    }
+    else if(percentage >= 50)
+    {
+        printf("Grade: D\n");
+    }
+    else {
+        printf("Grade: F\n");
+    }
 
+    
 
 
     
